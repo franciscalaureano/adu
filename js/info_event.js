abfef79d6786e36058da36ee5861075a6f373ab8
rafa000costa <rafa000costa@gmail.com>
2022-12-10 16:05:27 +0000

    tudo feito

--- js/info_event.js
+++ js/info_event.js
@@ -1,21 +1,21 @@
 //MUDAR A IMAGEM DAS SETAS DESCER E SUBIR
-/*const seta_descer = document.getElementById('seta_descer');
-let toggle = true;
-seta_descer.addEventListener('click', function(){
-    toggle = !toggle;
-    if(toggle){
-        seta_descer.src = "assets/img/seta_descer.png";
-    } else {
-        seta_descer.src = "assets/img/seta_subir.png";
-    }
-})*/
+const seta_descer = document.getElementById('seta_descer');
+seta_descer.addEventListener('click', function () {
+    const seta_subir = document.getElementById("seta_subir").style.display = "block";
+    seta_descer = document.getElementById("seta_descer").style.display = "none";
+})
+
+seta_subir.addEventListener('click', function () {
+    seta_subir = document.getElementById("seta_subir").style.display = "none";
+    seta_descer = document.getElementById("seta_descer").style.display = "block";
+})
 
 //MUDAR A IMAGEM DO ICON GO!
 const icon_go = document.getElementById('icon_go');
 let toggle2 = true;
-icon_go.addEventListener('click', function(){
+icon_go.addEventListener('click', function () {
     toggle2 = !toggle2;
-    if(toggle2){
+    if (toggle2) {
         icon_go.src = "assets/img/go.png";
     } else {
         icon_go.src = "assets/img/go_2.png";
@@ -25,9 +25,9 @@ icon_go.addEventListener('click', function(){
 //MUDAR A IMAGEM DO ICON CORACAO
 const icon_coracao = document.getElementById('icon_coracao');
 let toggle3 = true;
-icon_coracao.addEventListener('click', function(){
+icon_coracao.addEventListener('click', function () {
     toggle3 = !toggle3;
-    if(toggle3){
+    if (toggle3) {
         icon_coracao.src = "assets/img/coracao.png";
     } else {
         icon_coracao.src = "assets/img/coracao_2.png";
