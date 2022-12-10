2300dfd7dc2c706dcd5f8204464ad23690f66c22
isaroxo <isanarcisoroxo@gmail.com>
2022-12-10 15:49:11 +0000

    Continuação do js e algumas adições no css

--- js/events.js
+++ js/events.js
@@ -26,5 +26,65 @@ for( let i=0; i<buttons_heart.length; i++){
     })
 }
 
+//CLICAR NO BOTÃO DA SETA PARA ANDAR PARA TRÁS
+let button_arrow_back = document.getElementById('button_arrow_back');
+button_arrow_back.addEventListener('click', function(){
+    window.location.replace("home.html");
+})
+
+//CLICAR NO BOTÃO FILTER
+let button_filter = document.getElementById('button_filter');
+button_filter.addEventListener('click', function(){
+    console.log("div filter");
+})
 
 
+
+//BARRA DE PESQUISA
+document.addEventListener("keyup", function(event) {
+    search();
+});
+
+function search() {
+    let input = document.getElementById('input_search').value
+    input=input.toLowerCase();
+    let div_event_columbus_buttons_heart_go = document.getElementById('div_event_columbus_buttons_heart_go')
+    let image_event_walk = document.getElementById('image_event_walk')
+    let date_event_walk = document.getElementById('date_event_walk')
+    let title_event_walk = document.getElementById('title_event_walk')
+    let hours_event_walk = document.getElementById('hours_event_walk')
+    let local_event_walk = document.getElementById('local_event_walk')
+    let event_walk = document.getElementById('event_walk');
+
+    if (input == "walk" || input == "w" || input == "wa" || input == "wal" || input=="sport" || input == "fitness"){
+        div_event_columbus_buttons_heart_go.style.display="none";
+    }
+
+    else if (input == "t" || input == "ta" || input == "tau" || input == "tauc" || input=="music"){
+        div_event_columbus_buttons_heart_go.style.display="none";
+        image_event_walk.src="assets/img/event_tauc.png";
+        date_event_walk.innerHTML = "04th of december";
+        title_event_walk.innerHTML = "TAUC & Sofia Escobar Orchestra";
+        hours_event_walk.innerHTML = "06:00 pm - 07:00 pm";
+        local_event_walk.innerHTML = "Convento de São Francisco (1,5 km)";
+        if (event_walk != null) {
+            document.getElementById("event_walk").id = "event_tauc";
+        }
+        //CLICAR NO EVENTO TAUC
+        let event_tauc = document.getElementById('event_tauc');
+        event_tauc.addEventListener('click', function () {
+            window.location.replace("info_event.html");
+        })
+    }
+    else{
+        div_event_columbus_buttons_heart_go.style.display="flex";
+        image_event_walk.src="assets/img/event_walk.png";
+        date_event_walk.innerHTML = "04th of december";
+        title_event_walk.innerHTML = "WALK TO DEFEAT ALS® - NORTHEASTERN";
+        hours_event_walk.innerHTML = "08:30 am - 12:00 am";
+        local_event_walk.innerHTML = "Neuroscience Group Field - Fox Cities Stadium (3 km)";
+        if (event_tauc != null) {
+            document.getElementById("event_tauc").id = "event_walk";
+        }
+    }
+}
